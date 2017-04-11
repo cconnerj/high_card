a1647aac74ef49932fc25af6b9f9f33eb8a68eb0
Conner Johnson <Conner.johnson33@gmail.com>
2017-04-11 19:12:35 -0400

    deck shuffle

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,24 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+
+require 'pry'
+def deck
+  card = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  suits = ['hearts', 'diamonds', 'clubs', 'spades']
+  deck = []
+
+  card.each do |c|
+  suits.each do |s|
+    deck << ({
+      card: c,
+      suit: s,
+      })
+    end
+  end
+  deck.shuffle
+end
+
 players = []
+
+binding.pry
